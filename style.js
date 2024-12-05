ccab7b086c1eb429c39b4c05f06358864b644ae3
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2024-12-06 03:18:11 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,4 @@
+// this is the dummy file which is created to learn how to link the main html file to the js file.
+document.addEventListener("DOMContentLoaded", function(){
+    alert("Hello Ji Kaise Ho Saare !!");
+});
