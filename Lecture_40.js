81b0bf1cb7637ff705479716da61a394eba3d73f
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2025-01-08 22:54:01 +0530

    Add files via upload

---
+++ Lecture_40.js
@@ -0,0 +1,23 @@
+console.log("Learning about variables");
+// var age = 25; 
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 26; 
+//     console.log(age);
+// }
+
+//console.log(age);   we can't access the variable outside the function , but if we'll declare it globally we can use it throughout the program.
+// solve();
+
+// const a = 25; 
+//a = 35; we can't redefine the value of variable which is declared as constant , it's it will remain constant throughout the program. 
+
+// always use variable naming convention as camel case , and it should be meaningful.
+// also don't use the reserved keywords to declare the variables , as it will through error while running a program.
+
+
+
