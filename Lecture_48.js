c934357f41fbc6ae323ed4d5db7503446f674b3e
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2025-01-17 14:25:19 +0530

    Add files via upload

---
+++ Lecture_48.js
@@ -0,0 +1,80 @@
+console.log("Hello world !!"); 
+// we have math function which is object. 
+
+// Printing the value of PI . 
+console.log(Math.PI);
+
+console.log(Math.max(60,34,25,600,123,456));
+
+console.log(Math.min(60,34,25,600,123,456));
+
+console.log(Math.round(1.4)); 
+
+console.log(Math.floor(1.4)); 
+
+console.log(Math.ceil(1.4)); 
+
+console.log(Math.abs(-5)); 
+
+console.log(Math.random());
+
+
+// Date object : 
+let curr = new Date(); 
+console.log(curr);
+
+
+let date = new Date('June 20 1988 07:15'); 
+
+console.log(date); 
+
+let newDate = new Date(1988,5,20,7); 
+console.log(newDate); 
+
+
+console.log(newDate.setFullYear(2021));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
