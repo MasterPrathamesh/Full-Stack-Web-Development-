2ea8787cc3d6e223799c2c31298ec1da2fc6b71b
Prathamesh Deshmukh <109153108+MasterPrathamesh@users.noreply.github.com>
2025-01-20 12:31:09 +0530

    Add files via upload

---
+++ Lecture_51.js
@@ -0,0 +1,41 @@
+// JS DOM Manipulation : Access / Insert / Update / Delete elements . 
+
+// document.getElementById(); 2.document.getElementByClassName(); 3. document.getElementByTagName(); 
+
+// document.querySelector(); document.querySelectorAll(); 
+
+// basic window object , DOM(Document Object Model) , BOM(Browser Object Model) . 
+
+// Update existing element : 1. innerHTML 2.OuterHTML 3. text-content 4. innerText
+
+// Adding Element : 1.creatElement() 2.appendChild() 
+
+// insertAdjacentElement(); -----> position : 1. beforebegin 2.afterbegin 3.beforeend 4.afterend.
+
+//removeChild() : opposite of append ; 
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+
+parent.removeChild(child);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
